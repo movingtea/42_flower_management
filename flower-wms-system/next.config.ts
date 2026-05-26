594a353ec90e2b92e306b4a83b75bcdf9528e244
movingtea <kakuryou@gmail.com>
2026-05-26 20:15:15 +0800

    i: 首次打通 Docker 极致脱水打包与 GitHub Actions 生产级自动化发布链条

--- flower-wms-system/next.config.ts
+++ flower-wms-system/next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   allowedDevOrigins: ["192.168.31.200", "localhost:3000"],
   async redirects() {
     return [
